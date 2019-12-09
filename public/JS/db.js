be0f0f065b0fd837fb51f0e24a8c6ddb12fe3c19
Sam Levy <slevy239@gmail.com>
2019-12-08 22:36:42 -0500

    email validation

--- public/JS/db.js
+++ public/JS/db.js
@@ -1,45 +1,29 @@
 $(document).ready(function () {
 
 
-// $.get("/api/all", function (data) {
-//     if (data.length !== 0) {
-//         for (let i = 0; i < data.length; i++) {
-//             var row = $("<div>");
-//             row.addClass("contact");
+    $("#submit").on("click", function (event) {
+        event.preventDefault();
 
-//             row.append("<p>" + data[i].author + " contacted.. </p>");
-//             row.append("<p>" + data[i].body + "</p>");
-//             row.append("<p>At " + moment(data[i].created_at).format("h:mma on dddd") + "</p>");
+        let newContact = {
+            name: $("#name").val().trim(),
+            email: $("#email").val().trim(),
+            body: $("#body").val().trim(),
+            created_at: moment().format("YYYY-MM-DD HH:mm:ss")
+        }
+        var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
+        if (testEmail.test(newContact.email)) {
+            console.log(newContact);
+            $.ajax("/api/new", {
+                type: "POST",
+                data: newContact
+            }).then(function (data) {
+                location.reload();
+                alert("Thank you for your submission!")
+            })
 
-//             // $("#chirp-area").prepend(row);
+        }
 
-
-//         }
-//     }
-// })
-
-
-
-$("#submit").on("click", function (event) {
-    event.preventDefault();
-
-    let newContact = {
-        name: $("#name").val().trim(),
-        email: $("#email").val().trim(),
-        body: $("#body").val().trim(),
-        created_at: moment().format("YYYY-MM-DD HH:mm:ss")
-    }
-    console.log(newContact);
-$.ajax("/api/new", {
-    type: "POST",
-    data: newContact
-}).then(function (data) {
-    location.reload();
-    alert("Thank you for your submission!")
-})
-
-    // $("#email").val("");
-    // $("#body").val("");
-})
-
+        else
+            alert("Please enter a valid email address!")
+    })
 })

3af18219ebf226f9d24a9825854277aaf94a3c44
Sam Levy <slevy239@gmail.com>
2019-12-09 16:17:35 -0500

    server.js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require("express");
-
 var PORT = process.env.PORT || 8080;
-
 var app = express();
+var path = require('path');
+
+app.use('/public', express.static(path.join(__dirname + '/public')));
+
+
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -23,7 +26,7 @@ var routes = require("./controllers/contactController.js");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
